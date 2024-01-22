7c9a442c5a5c9830b0b70520094accc5dd5e7d77
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-01-22 20:07:53 -0300

    add movie page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
-import { FcRating } from "react-icons/fc";
-import { MdSupervisorAccount } from "react-icons/md";
-
-
+import { FcRating } from 'react-icons/fc'
+import { MdSupervisorAccount } from 'react-icons/md'
 
 const MoviePage = async ({ params }) => {
   const movieId = params.id
@@ -19,11 +17,11 @@ const MoviePage = async ({ params }) => {
 
 
   return (
-    <div className="flex bg-white/10 border border-gray-700/50 rounded-md max-w-6xl mx-auto mt-12 py-8 px-4">
-      <figure>
+    <div className="grid grid-cols-2 max-md:grid-cols-1 p-16 bg-white/10 border border-gray-700/50 rounded-md max-w-6xl mx-auto mt-12 py-8 px-4">
+      <figure className="flex-1 flex items-center justify-center">
         <Image
-          src={`https://image.tmdb.org/t/p/original/${
-            movie?.backdrop_path || movie?.poster_path
+          src={`https://image.tmdb.org/t/p/original${
+            movie?.backdrop_path || movie?.poster_path 
           }`}
           alt={movie?.title}
           width={500}
@@ -32,10 +30,24 @@ const MoviePage = async ({ params }) => {
         />
       </figure>
       <div className="flex flex-col flex-1 gap-4 p-4">
-        <h1 className="text-xl font-bold text-center ">{movie?.title}</h1>
-        <p className="text-sm text-justify indent-8">{movie?.overview}</p>
-        <p className="text-sm flex items-center gap-1"><FcRating/>{movie?.vote_average}</p>
-        <p className="text-sm flex items-center gap-1"><MdSupervisorAccount size={25} /> {movie?.popularity }</p>
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="text-xl font-bold text-center ">{movie?.title}</h1>
+          <p className="text-sm text-justify flex justify-center indent-8 max-md:w-5/6">
+            {movie?.overview}
+          </p>
+        </div>
+        <div className="flex gap-2 justify-around max-md:flex md:flex-col">
+          <p className="text-sm flex items-center gap-1">
+            <FcRating />
+            {movie?.vote_average}
+          </p>
+          <p className="text-sm flex items-center gap-1">
+            <MdSupervisorAccount size={25} /> {movie?.popularity}
+          </p>
+          <div className='flex gap-2'>Genres: {movie?.genres.map((genre) => (
+            <p key={genre.id} className="text-sm flex items-center gap-1 underline underline-offset-8" >{genre.name}</p>
+          ))}</div>
+        </div>
       </div>
     </div>
   )
