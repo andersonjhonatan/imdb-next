332fa1ac82c8c07dbd40ca9bb5900828dd9fa7d1
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-01-22 21:25:11 -0300

    add about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,9 +7,9 @@ function AboutPage() {
   return (
     <div>
       <AboutComponent
-        title1="1"
-        title2="2"
-        title3="3"
+        title1="Bem-vindo ao nosso IMDb Clone, o seu refúgio digital para uma experiência cinematográfica única! Nosso site foi meticulosamente projetado para proporcionar a você uma jornada envolvente pelo vasto mundo do cinema. Embora atualmente estejamos focados na visualização de informações, estamos trabalhando incessantemente nos bastidores para oferecer em breve funcionalidades de interação e compartilhamento"
+        title2="O propósito principal do nosso site é proporcionar a você uma plataforma intuitiva e abrangente para explorar filmes de maneira detalhada. Navegue por uma extensa coleção de obras cinematográficas, desde os clássicos intemporais até os lançamentos mais recentes. Descubra informações detalhadas sobre elenco, direção, sinopse e avaliações críticas, tudo apresentado de forma visualmente cativante"
+        title3="Embora, atualmente, não seja possível compartilhar diretamente suas descobertas, acreditamos que a verdadeira essência do cinema está na apreciação individual. Use nosso site como seu guia pessoal para a sétima arte, onde cada página é uma janela para a magia cinematográfica. Estamos comprometidos em aprimorar continuamente a experiência do usuário, adicionando em breve funcionalidades de compartilhamento para que você possa compartilhar suas paixões cinematográficas com outros amantes de filmes"
         address="Home"
         Icon={ImHome}
       />
--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -3,13 +3,14 @@
 import Link from 'next/link'
 import React from 'react'
 
-function AboutComponent({ title1, title2, title3}) {
+function AboutComponent({ title1, title2, title3 }) {
   return (
-    <div className="flex justify-center">
-      <div>
-        <Link href="/" className="text-3xl hover:text-amber-600">{title1}</Link>
-        <Link href="/" className="text-3xl hover:text-amber-600">{title2}</Link>
-        <Link href="/" className="text-3xl hover:text-amber-600">{title3}</Link>
+    <div className="flex justify-center w-full " >
+      <div className="flex flex-col gap-6 p-4 max-w-6xl mx-auto">
+        <h1 className="text-4xl font-bold text-center">About IMDb</h1>
+        <p className="text-md indent-8 text-justify">{title1}</p>
+        <p className="text-md indent-8 text-justify">{title2}</p>
+        <p className="text-md indent-8 text-justify">{title3}</p>
       </div>
     </div>
   )
